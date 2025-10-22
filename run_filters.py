ea5a7dd8cdff83b69709eb4e83a55223eb69701b
eline-dn <eline.denis@gmail.com>
2025-10-22 12:10:04 +0200

    Update run_filters.py

--- run_filters.py
+++ run_filters.py
@@ -45,6 +45,7 @@ for i in cond_list:
   #filtering
   df=compute_specific_metrics(df)
   df=compute_nonspecific_metrics(df)
+  df=compute_cofolding_metrics(df)
   ## filters on non_specific reprediction scores
   filtered_df_non_specific = df[(df['mean_empty_i_pTM'] <0.5) & (df['mean_plugged_i_pTM'] >=0.8) & (df['interface_dSASA']>=1700)]
   ## filters on specific reprediction scores
@@ -89,7 +90,51 @@ plt.savefig(f"{output_folder}/pairplot_iptms2.png")
 
 
 
+# plot the ipTM scatter plots with thresholds and compare them, highlight selected binders from the first round of selection
+selected1_binder_list=global_df[(global_df['specific_mean_empty_i_pTM'] <0.5) & (global_df['specific_mean_plugged_i_pTM'] >=0.8)& (global_df['interface_dSASA']>=1700)].Design
+global_df['selected1'] = global_df['Design'].isin(selected1_binder_list)
 
+# scatter plot for case n°1
+_=sns.scatterplot(data=global_df, x='specific_mean_plugged_i_pTM', y='specific_mean_empty_i_pTM',hue='selected1')
+plugged_ipTM = 0.8
+plt.axvline(x=plugged_ipTM, ymin=0, ymax=1, color="black", linestyle="--")
+empty_ipTM = 0.5
+plt.axhline(
+    y=empty_ipTM, xmin=0, xmax=1, color="black", linestyle="--"
+)
+plt.xlabel("Plugged i_pTM ")
+plt.ylabel("Empty i_pTM")
+plt.title("ipTM Scatterplot for the ipTM values from the reprediction with target from BC (case 1)")
+plt.legend(title="Binders selected with case 1 filters")
+plt.savefig("{output_folder}/scatter_1.png")
+
+# scatter plot for case n°2
+_=sns.scatterplot(data=global_df, x='mean_plugged_i_pTM', y='mean_empty_i_pTM',hue='selected1')
+plugged_ipTM = 0.8
+plt.axvline(x=plugged_ipTM, ymin=0, ymax=1, color="black", linestyle="--")
+empty_ipTM = 0.5
+plt.axhline(
+    y=empty_ipTM, xmin=0, xmax=1, color="black", linestyle="--"
+)
+plt.xlabel("Plugged i_pTM ")
+plt.ylabel("Empty i_pTM")
+plt.title("ipTM Scatterplot for the ipTM values from the reprediction with initial target (case 2)")
+plt.legend(title="Binders selected with case 1 filters")
+plt.savefig("{output_folder}/scatter_2.png")
+
+# scatter plot for case n°3
+_=sns.scatterplot(data=global_df, x='cofold_mean_plugged_i_pTM', y='cofold_mean_empty_i_pTM',hue='selected1')
+plugged_ipTM = 0.8
+plt.axvline(x=plugged_ipTM, ymin=0, ymax=1, color="black", linestyle="--")
+empty_ipTM = 0.5
+plt.axhline(
+    y=empty_ipTM, xmin=0, xmax=1, color="black", linestyle="--"
+)
+plt.xlabel("Plugged i_pTM ")
+plt.ylabel("Empty i_pTM")
+plt.title("ipTM Scatterplot for the ipTM values from the reprediction without target template (case 3)")
+plt.legend(title="Binders selected with case 1 filters")
+plt.savefig("{output_folder}/scatter_3.png")
 
 
 
